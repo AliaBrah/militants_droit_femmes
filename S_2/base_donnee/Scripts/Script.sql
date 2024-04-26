da043aeded05a7f0f097f71d6397ddd02e556011
Alia Brahmi <146718686+AliaBrah@users.noreply.github.com>
2024-04-26 16:01:56 +0200

    script essai

--- S_2/base_donnee/Scripts/Script.sql
+++ S_2/base_donnee/Scripts/Script.sql
@@ -7,6 +7,9 @@ SELECT p.pk
 FROM personne p 
 WHERE genre = 'trans woman';
 
+
+-- création d'une vue
+
 CREATE VIEW effectif_occupation
 AS
 SELECT lpo.fk_occupation, COUNT(*) AS eff,  lpo.occupation 
@@ -14,6 +17,8 @@ FROM liaison_pers_occup lpo
 GROUP by lpo.fk_occupation
 HAVING count(*) > 1
 ORDER BY lpo.occupation  ;
+
+-- essais pour avoir les effectifs par occupations
 
 SELECT COUNT (DISTINCT Fk_personne)
 FROM liaison_pers_occup lpo ;
@@ -23,15 +28,17 @@ FROM liaison_pers_occup lpo
 GROUP BY lpo.occupation
 ORDER BY lpo.occupation ;
 
+-- essai pour additionner les effectifs dans des classes d'occupation qui se ressemble
+
 SELECT
 SUM(eff) AS enseignant
 FROM effectif_occupation eo 
-WHERE (eo.occupation  = 'teacher')OR (eo.occupation ='university teacher')
+WHERE (eo.occupation  = 'teacher')OR (eo.occupation ='university teacher') -- les enseignants ensemble
 UNION
 SELECT
 SUM(eff) AS artistes
 FROM effectif_occupation eo 
-WHERE (eo.occupation  = 'painter')OR (eo.occupation ='artist') OR (eo.occupation ='actor');
+WHERE (eo.occupation  = 'painter')OR (eo.occupation ='artist') OR (eo.occupation ='actor'); -- les artistes ensemble
 
 
 
