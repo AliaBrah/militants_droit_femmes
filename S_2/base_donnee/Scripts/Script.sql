1174a9fab787cc0458a095fa9f4f182e5e74103f
Alia Brahmi <146718686+AliaBrah@users.noreply.github.com>
2024-05-03 15:34:26 +0200

    changement de population

--- S_2/base_donnee/Scripts/Script.sql
+++ S_2/base_donnee/Scripts/Script.sql
@@ -5,7 +5,7 @@ HAVING count(*) > 1;
 
 SELECT p.pk
 FROM personne p 
-WHERE genre = 'trans woman';
+WHERE genre = 'non-binary'  ;
 
 
 -- création d'une vue qui compte les effectif par occupation
@@ -13,18 +13,18 @@ WHERE genre = 'trans woman';
 CREATE VIEW effectif_occupation
 AS
 SELECT lpo.fk_occupation, COUNT(*) AS eff,  lpo.occupation 
-FROM liaison_pers_occup lpo 
+FROM liaison_personne_occupation lpo 
 GROUP by lpo.fk_occupation
 HAVING count(*) > 1
-ORDER BY lpo.occupation  ;
+ORDER BY lpo.occupation;
 
 -- essais pour avoir les effectifs par occupations
 
 SELECT COUNT (DISTINCT Fk_personne)
-FROM liaison_pers_occup lpo ;
+FROM liaison_personne_occupation   lpo ;
 
 SELECT lpo.occupation, count(*)AS eff, lpo.occupation 
-FROM liaison_pers_occup lpo 
+FROM liaison_personne_occupation   lpo 
 GROUP BY lpo.occupation
 ORDER BY lpo.occupation ;
 
@@ -76,15 +76,15 @@ SUM(eff) AS artistes
 FROM effectif_occupation eo 
 WHERE (eo.occupation  = 'painter')OR (eo.occupation ='artist') OR (eo.occupation ='actor'); -- les artistes ensemble /problème création d'une ligne et non d'une colonne
 
-SELECT iwn.column1, count(*) AS eff  --263 qui ont plusieurs nationalités
-FROM import_wiki_nationalite iwn 
-GROUP BY iwn.column1 
-HAVING count(*) > 1; --263 qui ont plusieurs nationalités
+SELECT win.column1, count(*) AS eff  --757 qui ont plusieurs nationalités
+FROM wiki_import_nationalite win  
+GROUP BY win.column1 
+HAVING count(*) > 1; --757 qui ont plusieurs nationalités
 
-SELECT iwn.column1, count(*) AS eff, GROUP_CONCAT(iwn.column5) AS nationalites -- les deux nationalités sortent !!!!!
-FROM import_wiki_nationalite iwn 
-GROUP BY iwn.column1 
-HAVING count(*) > 1; --263 qui ont plusieurs nationalités
+SELECT win.column1, count(*) AS eff, GROUP_CONCAT(win.column5) AS nationalites -- les deux nationalités sortent !!!!!
+FROM wiki_import_nationalite win  
+GROUP BY win.column1 
+HAVING count(*) > 1; --757 qui ont plusieurs nationalités
 
 SELECT lpo.Fk_personne, count(*) AS eff  --2173 qui ont plusieurs occupations
 FROM liaison_pers_occup lpo 
@@ -94,7 +94,7 @@ HAVING count(*) > 1;
 -- requête pour démontrer les combinaisons d'occupations
 
 SELECT lpo.Fk_personne, count(*) AS eff, GROUP_CONCAT(lpo.occupation) as occupations --2173 qui ont plusieurs occupations
-FROM liaison_pers_occup lpo 
+FROM liaison_personne_occupation   lpo 
 GROUP BY lpo.Fk_personne 
 HAVING count(*) > 1; 
 
